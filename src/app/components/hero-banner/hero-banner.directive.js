62593165771abe55e44846454dcd06e108268413
Virginia Cepeda <virginiamcepeda@gmail.com>
2015-09-30 20:55:54 -0300

    feat(index): add routes

---
+++ src/app/components/hero-banner/hero-banner.directive.js
@@ -0,0 +1,29 @@
+/**
+ * Created by vika on 30/09/15.
+ */
+(function() {
+	'use strict';
+
+	angular
+		.module('Main')
+		.directive('heroBanner', heroBanner);
+
+	/** @ngInject */
+	function heroBanner() {
+		var directive = {
+			restrict: 'E',
+			templateUrl: 'app/components/hero-banner/hero-banner.html',
+			scope: {
+				title: '='
+			},
+			link: NavbarLink
+		};
+
+		return directive;
+
+		/** @ngInject */
+		function NavbarLink() {
+		}
+	}
+
+})();
--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,20 +1,39 @@
 (function() {
-  'use strict';
+	'use strict';
 
-  angular
-    .module('Main')
-    .config(routeConfig);
+	angular
+		.module('Main')
+		.config(routeConfig);
 
-  function routeConfig($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'main'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  }
-
+	function routeConfig($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'app/main/main.html',
+				controller: 'MainController',
+				controllerAs: 'mainController'
+			})
+			.when('/blog', {
+				templateUrl: 'app/blog/blog.html',
+				controller: 'blogCtrl',
+				controllerAs: 'blogController'
+			})
+			.when('/contact', {
+				templateUrl: 'app/main/main.html',
+				controller: 'MainController',
+				controllerAs: 'contactController'
+			})
+			.when('/events', {
+				templateUrl: 'app/events/events.html',
+				controller: 'eventsCtrl',
+				controllerAs: 'eventsController'
+			})
+			.when('/news', {
+				templateUrl: 'app/news/news.html',
+				controller: 'newsCtrl',
+				controllerAs: 'newsController'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	}
 })();
